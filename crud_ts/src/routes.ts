c834d7c4400f66f39e004c11809528d39445ac3d
Igor Martins <lima.igormartins@gmail.com>
2024-09-02 19:01:27 -0300

    atualização

--- crud_ts/src/routes.ts
+++ crud_ts/src/routes.ts
@@ -43,7 +43,7 @@ router.get('/leitura', (req, res) => {
       return res.status(500).send('Internal Server Error');
     }
 
-    if (result.length === 0) {
+    else {
       return res.status(404).send(result);
     }
 
